dfaca5e5d4746547691470c5b98569e83259f9aa
jessicasouzajds <jessica.souzajds@gmail.com>
2021-04-28 22:18:06 -0700

    Adding PPG sensing.

--- read_force_ppg/read_force_ppg.ino
+++ read_force_ppg/read_force_ppg.ino
@@ -5,14 +5,12 @@ MAX30105 particleSensor;
 
 void setup(){
   Wire.begin(); // join i2c bus (address optional for master)
-  Serial.begin(115200);  // 57600start serial for output
+  Serial.begin(57600);  // 57600start serial for output
 
   short data = readDataFromSensor(0x04);
   // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_STANDARD)){ //Use default I2C port, 400kHz speed or I2C_SPEED_STANDARD
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
-    while (1);
-  }
+  particleSensor.begin(Wire, I2C_SPEED_STANDARD);
+  
   //Setup to sense a nice looking saw tooth on the plotter
   byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA     //LED Pulse Amplitude Configuration
   //0x1F = 6.4mA, 0x02 = 0.4mA, 0x7F = 25.4mA, 0xFF= 50.0mA
@@ -21,17 +19,9 @@ void setup(){
   byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
   int sampleRate = 50; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200 samples per second
   int pulseWidth = 411; //Options: 69, 118, 215, 411
-  int adcRange = 2048; //Options: 2048, 4096, 8192, 16384
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
   //18 bit resolution
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
-
-  //Take an average of IR readings at power up
-  const byte avgAmount = 64;
-  long baseValue = 0;
-  for (byte x = 0 ; x < avgAmount ; x++){
-    baseValue += particleSensor.getRed(); //Read the IR value
-  }
-  baseValue /= avgAmount;
 
 }
 
@@ -39,9 +29,12 @@ void loop(){
     byte i2cAddress = 0x04; // Slave address (SingleTact), default 0x04
     short data = readDataFromSensor(i2cAddress);
     float fsrVoltage;       // the analog reading converted to voltage
+    float parVoltage;       // the analog reading converted to voltage
     float force;
     float vout;
-       
+    float voutppg;
+    String outt;
+  
     fsrVoltage = map(data, 0, 1022, 0, 2000);
     vout = fsrVoltage/1000;
 
@@ -55,9 +48,11 @@ void loop(){
       vout = 0;
     }
     
-    Serial.print(vout);
-    Serial.print(',');
-    Serial.println(particleSensor.getRed()); //Send raw data to plotter  FIX amplitude!!!
+    parVoltage = particleSensor.getRed();
+    voutppg = parVoltage/100;
+    outt = String(vout) + ',' + voutppg;
+    Serial.println(outt);
+  
     delay(100); // Change this if you are getting values too quickly 
     // Different sampling rate for sending over serial
 }
